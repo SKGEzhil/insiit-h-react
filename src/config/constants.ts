76c90fb1d20ae86b34e082bc7e7578f86179b63a
SKGEzhil <skgezhil2005@gmail.com>
2025-02-09 14:07:32 +0530

    fix: to get backend url from env

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,7 +1,13 @@
 // export const endPoint = 'http://localhost:4000/graphql';
 
-export const endPoint = 'http://localhost:4000/graphql';
-export const  endPointBase = 'http://localhost:4000';
+// export const endPoint = 'http://localhost:4000/graphql';
+// export const  endPointBase = 'http://localhost:4000';
+
+
+export const endPoint = import.meta.env.VITE_ENDPOINT;
+export const  endPointBase = import.meta.env.VITE_ENDPOINTBASE;
+
+console.log('endPoint', endPoint);
 
 // export const endPoint = 'http://10.8.91.51:4000/graphql';
 // export const  endPointBase = 'http://10.8.91.51:4000/';
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
+import { config } from 'dotenv';
 import react from '@vitejs/plugin-react'
+
+config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
     'global': {},
+    'process.env': process.env
   },
   // server: {port: 80}
 })
